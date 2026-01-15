e87f7f6be286bde03c32a421f9aa1556283dca8b
Ivan Lopez <ivan.lopez>
2026-01-15 13:04:53 -0600

    Initial Commit

---
+++ QuickExample/QuickExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickExampleApp.swift
+//  QuickExample
+//
+//  Created by Ivan Lopez on 15/01/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
