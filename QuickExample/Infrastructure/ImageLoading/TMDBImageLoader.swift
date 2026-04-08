8bcab6b21f5d478428ba2545c96559cfe1bf35b6
Ivan Lopez <ivan.lopez>
2026-04-08 14:40:46 -0600

    Add inFlightTasks to avoid multiple networkrequests

--- QuickExample/Infrastructure/ImageLoading/TMDBImageLoader.swift
+++ QuickExample/Infrastructure/ImageLoading/TMDBImageLoader.swift
@@ -19,23 +19,40 @@ actor TMDBImageLoader: ImageLoading {
     private let commonHeaders = ["Authorization": "Bearer \(Secrets.tmdbApiKey)"]
     
     private var cache: [String: UIImage] = [:]
+    private var inFlightTasks: [String: Task<UIImage, Error>] = [:]
     
     func loadImage(from posterPath: String) async throws -> UIImage {
         if let cached = cache[posterPath]  {
             return cached
         }
         
-        let request = try await ImageRequestBuilder.makeRequest(
-            baseURL: .imageBaseUrl,
-            endpoint: .poster(path: posterPath, size: .w500),
-            headers: commonHeaders
-        )
-        let (data, _) = try await URLSession.shared.data(for: request)
-        guard let image = UIImage(data: data) else {
-            throw ImageLoadingError.invalidImageData
+        if let existingTask = inFlightTasks[posterPath] {
+            return try await existingTask.value
         }
         
-        cache[posterPath] = image
-        return image
+        let task = Task<UIImage, Error> {
+            let request = try await ImageRequestBuilder.makeRequest(
+                baseURL: .imageBaseUrl,
+                endpoint: .poster(path: posterPath, size: .w500),
+                headers: commonHeaders
+            )
+            let (data, _) = try await URLSession.shared.data(for: request)
+            guard let image = UIImage(data: data) else {
+                throw ImageLoadingError.invalidImageData
+            }
+            return image
+        }
+        
+        inFlightTasks[posterPath] = task
+        
+        do {
+            let image = try await task.value
+            cache[posterPath] = image
+            inFlightTasks[posterPath] = nil
+            return image
+        } catch {
+            inFlightTasks[posterPath] = nil
+            throw error
+        }
     }
 }
