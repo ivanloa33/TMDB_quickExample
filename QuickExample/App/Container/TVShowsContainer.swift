843db0b7c1b075782290aee04b491ac2ba0b3546
Ivan Lopez Ansaldo <ivan.loa.33@gmail.com>
2026-05-12 17:01:13 -0600

    Add cache for tvShowSection

--- QuickExample/App/Container/TVShowsContainer.swift
+++ QuickExample/App/Container/TVShowsContainer.swift
@@ -10,19 +10,30 @@ import SwiftUI
 final class TVShowsContainer {
     private let core: CoreContainer
     
+    // MARK: - Not creating TVShowSectionCacheKey and CacheTVShow types
+    // Keeping simpler and want to see when is necesarry dedicated CacheTypes
+    private let cacheTVShowSection: any Cache<TVShowCategory, [TVShowDTO]>
+    
     private let repository: TVShowsRepository
     private let fetchTVShowsUseCase: FetchTVShowsUseCase
     
     init(core: CoreContainer) {
         self.core = core
         
-        let repository = TVShowsRepositoryImpl(httpClient: core.httpClient)
+        let cacheTVShowSection = InMemoryCache<TVShowCategory, [TVShowDTO]>()
+        self.cacheTVShowSection = cacheTVShowSection
+        
+        let repository = TVShowsRepositoryImpl(
+            httpClient: core.httpClient,
+            cache: cacheTVShowSection
+        )
         
         self.repository = repository
-        
         self.fetchTVShowsUseCase = FetchTVShowsUseCaseImpl(repository: repository)
     }
-    
+}
+
+extension TVShowsContainer {
     func makeTVShowListView(
         _ category: TVShowCategory,
         onTVShowTap: @escaping (Int) -> Void
--- QuickExample/Data/Repository/TVShowsRepositoryImpl.swift
+++ QuickExample/Data/Repository/TVShowsRepositoryImpl.swift
@@ -10,13 +10,25 @@ import Foundation
 final class TVShowsRepositoryImpl: TVShowsRepository {
     
     private let httpClient: HTTPClient
+    private let cache: any Cache<TVShowCategory, [TVShowDTO]>
     
-    init(httpClient: HTTPClient) {
+    init(httpClient: HTTPClient,
+         cache: any Cache<TVShowCategory, [TVShowDTO]>
+    ) {
         self.httpClient = httpClient
+        self.cache = cache
     }
     
     func fetchTVShows(category: TVShowCategory) async throws -> [TVShow] {
+        let key = category
+        
+        if let cacheTVShows = await cache.get(key: key) {
+            return cacheTVShows.map { $0.toDomain() }
+        }
+        
         let responseTVShowDTO: ResponseTVShowDTO = try await httpClient.get(endPoint: endpoint(category: category))
+        
+        await cache.set(responseTVShowDTO.results, for: key)
         
         return responseTVShowDTO.results.map { $0.toDomain() }
     }
